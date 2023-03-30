4fd3b4445a94993362be3db823194e2a6d82cb04
Mimimarroun <meriemmarroun@icloud.com>
2023-03-30 16:10:24 -0700

    magic tasks

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,9 +1,13 @@
-#include <stdio.h>
-
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
-int count_str(char *str);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
-int  _strcmp(char *s1, char *s2);
-int char_count(char *str);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *str);
+char *rot13(char *str);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
