b7104dee7909a1ccc01cf967eda876058cfa80c3
Mimimarroun <meriemmarroun@icloud.com>
2023-04-01 04:56:04 -0700

    last chance

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,42 +1,19 @@
-#include "main.h"
-
 /**
- * _strncat - Concatenates two strings
- * @dest: first str
- * @src: second str
- * @n: how many bytes to use
- * Return: Concatenated str
+ * _strncat - Concatenates two strings using at most
+ *            an inputted number of bytes from src.
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
+ *
+ * Return: A pointer to the resulting string dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int count;
-	int i;
+	int index = 0, dest_len = 0;
 
-	count = count_str(dest);
-	for (i = 0; src[i] != '\0' && i < n; i++)
-	{
-		dest[count] = src[i];
-		count++;
-	}
-	dest[count] = '\0';
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
-}
-
-/**
- * count_str - count how many letters are in a str
- * @str: str to count its letter
- * Return: How many letters are there
- */
-
-int count_str(char *str)
-{
-	int counter;
-	int i;
-
-	counter = 0;
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		counter++;
-	}
-	return (counter);
 }
