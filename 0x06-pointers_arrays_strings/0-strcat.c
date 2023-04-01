14b070b2eb40b58bb4dd5b26a718a962b88a0639
Mimimarroun <meriemmarroun@icloud.com>
2023-04-01 04:51:31 -0700

    last chance

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,41 +1,18 @@
-#include "main.h"
-
 /**
- * _strcat - Concatenates two strings
- * @dest: first str
- * @src: second str
- * Return: Concatenated str
+ * *_strcat - concatenates @src to @dest
+ * @src: the source string to append to @dest
+ * @dest: the destiation string to be concatenated upon
+ * Return:pointer to the resulting string
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int count;
-	int i;
+	int index = 0;
+	int dest_len = 0;
 
-	count = count_str(dest);
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[count] = src[i];
-		count++;
-	}
-	dest[count] = '\0';
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
-}
-
-/**
- * count_str - count how many letters are in a str
- * @str: str to count its letter
- * Return: How many letters are there
- */
-
-int count_str(char *str)
-{
-	int counter;
-	int i;
-
-	counter = 0;
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		counter++;
-	}
-	return (counter);
 }
