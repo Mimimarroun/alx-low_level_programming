10d13f40e1d4a9c409837a9e12f5ac45830b968d
Mimimarroun <meriemmarroun@icloud.com>
2023-04-03 15:57:49 -0700

    more pointers ...

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* @s: gets the length of a prefix substring
+* @accept: input
+* Return: the number of bytes in the initial
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int c = 0;
+	char *t = accept;
+
+	while (*s++)
+	{
+		while (*accept++)
+			if (*(s - 1) == *(accept - 1))
+			{
+				c++;
+				break;
+			}
+		if (!(*--accept))
+			break;
+		accept = t;
+	}
+	return (c);
+}
