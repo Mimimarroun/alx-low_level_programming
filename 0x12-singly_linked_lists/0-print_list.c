aba7d0313505d3d7a94f3d69ddbf4eec3b1080aa
Mimimarroun <meriemmarroun@icloud.com>
2023-04-28 12:19:47 -0700

    more add node .....

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,16 +9,16 @@
 
 size_t print_list(const list_t *h)
 {
-	int i;
-	char *str1;
+	int i = 0;
 
-	for (i = 0; h; i++, h = h->next)
+	while (h)
 	{
-		str1 = h->str;
-		if (str1)
-			printf("[%u] %s\n", h->len, str1);
+		if (h->str)
+			printf("[%u] %s\n", h->len, h->str);
 		else
-			printf("[0] %p\n", str1);
+			printf("[0] (nil)\n");
+		i++;
+		h = h->next;
 	}
 	return (i);
 }
