6862a9dd68e612f0b5b6d1856f6024f2496c658f
Mimimarroun <meriemmarroun@icloud.com>
2023-05-12 16:26:18 -0700

    last commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,12 +4,7 @@
 #include <stddef.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <elf.h>
 
-#define BUFSIZE 1204
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
